0b517cf0125a52feb3118454ab6be1fff2be64bb
daigo3 <stain.witness@gmail.com>
2016-11-08 00:06:52 +0900

    [done] async behavior

--- app/async.js
+++ app/async.js
@@ -2,10 +2,24 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise(resolve => resolve(value));
   },
 
   manipulateRemoteData: function(url) {
-
+    return new Promise((resolve, reject) => {
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.onload = () => {
+        if(xhr.status === 200) {
+          resolve(xhr.response);
+        } else {
+          reject(xhr.statusText);
+        }
+      };
+      xhr.send();
+    }).then((res) => {
+      let data = JSON.parse(res);
+      return data.people.map((item) => { return item.name; }).sort();
+    });
   }
 };
