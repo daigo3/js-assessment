171cb43a07e0d4eabbbbb7702375830c452fc8ef
daigo3 <stain.witness@gmail.com>
2016-11-08 00:13:16 +0900

    [done] count

--- app/count.js
+++ app/count.js
@@ -2,6 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    let counter;
 
+    let _count = function _count() {
+      console.log(start++);
+
+      if(start <= end) {
+        counter = setTimeout(_count, 100);
+      }
+    }
+
+    _count();
+
+    return {
+      cancel: function() {
+        counter && clearTimeout(counter);
+      }
+    }
   }
 };
